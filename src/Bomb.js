9ae4f585ce496faa3ad8c8a1e7c6cb0b7b29b8da
Thomas_Hansen <ctrhansen@yahoo.com>
2019-06-05 10:11:01 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,42 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+    constructor(props){
+        super(props)
+        console.log(props)
+        this.state = {
+            secondsLeft: this.props.initialCount
+        }
+    }
+
+    render() {
+
+        const countdown = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`    
+
+        return(
+            <div>{countdown}</div> 
+        )
+    }
+  
+    // constructor(props) {
+    //     super(props)
+    //     console.log(props)
+    //     this.state = {
+    //         color: this.props.value
+    //     }
+    // }
+
+    // changeColor = () => {
+    //     {this.setState({color: '#333'})}
+    // }
+
+    // render() {
+    //     return (
+    //         <div className='cell' 
+    //             style={{backgroundColor: this.state.color}} 
+    //             onClick={() => this.changeColor()}>
+    //         </div>
+    //     )
+    }
